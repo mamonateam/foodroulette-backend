4a1704c01e8fb8fa05a88d14d7f82293db58d79a
MiguelCarranza <miguel@mindsnacks.com>
2013-11-16 18:20:08 -0800

    Add cors middleware

--- foodroulette_backend/base_settings.py
+++ foodroulette_backend/base_settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cors.middleware.AllowOriginMiddleware',
 )
 
 ROOT_URLCONF = 'foodroulette_backend.urls'
